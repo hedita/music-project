c9f041848d2b371d2c2b0c2e3190f4255f31c389
Hedita <hedi.takrami@gmail.com>
2023-04-15 15:21:01 +0330

    configurition

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+
+const App = () => {
+  return <div>hi</div>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
