795a7f833455756e8afa43d58a8b8575a9633ea7
Hedita <hedi.takrami@gmail.com>
2023-04-16 19:17:12 +0330

    add style too music list

--- src/components/MusicList/MusicList.jsx
+++ src/components/MusicList/MusicList.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import "./MusicList.scss"
 
 const MusicList = () => {
   return (
     <ul className="music-list">
       <li>music</li>
+      <li>music</li>
+      <li>music</li>
+      <li>music</li>
+      <li>music</li>
+			<li>music</li>
       <li>music</li>
       <li>music</li>
       <li>music</li>
