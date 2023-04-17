d1b6e9645224419445454f4a3c088d439298d31b
Hedita <hedi.takrami@gmail.com>
2023-04-17 12:19:59 +0330

    fix the indentation

--- src/pages/Singles/Singles.jsx
+++ src/pages/Singles/Singles.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
 const Singles = () => {
- return (
-  <ul className="music-list">
-  <li>music</li>
-  <li>music</li>
-  <li>music</li>
-  <li>music</li>
-  <li>music</li>
-  <li>music</li>
-  <li>music</li>
-  <li>music</li>
-  <li>music</li>
-  <li>music</li>
-</ul>
- )
+  return (
+    <ul className="music-list">
+      <li>music</li>
+      <li>music</li>
+      <li>music</li>
+      <li>music</li>
+      <li>music</li>
+      <li>music</li>
+      <li>music</li>
+      <li>music</li>
+      <li>music</li>
+      <li>music</li>
+    </ul>
+  );
 };
 
 export default Singles;
