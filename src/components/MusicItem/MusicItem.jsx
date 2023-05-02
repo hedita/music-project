60254c1c369b41bd12eb104adfc32928f3447ec1
Hedita <hedi.takrami@gmail.com>
2023-05-02 12:34:18 +0330

    remove extra variables

--- src/components/MusicItem/MusicItem.jsx
+++ src/components/MusicItem/MusicItem.jsx
@@ -5,7 +5,7 @@ import "./MusicItem.scss";
 const MusicItem = () => {
   const musicList = list.albums.items;
 
-  const musics = musicList.map(({ name, id, images, artists }) => {
+  const musics = musicList.map(({ name, id, images }) => {
     return (
       <li key={id} className="music-item">
         <figure>
