290403985e55048ccf7dfb6424e07a57ec9f3935
Hedita <hedi.takrami@gmail.com>
2023-05-03 11:49:02 +0330

    refactore the code

--- src/components/PlayList/PlayList.jsx
+++ src/components/PlayList/PlayList.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import PlayListItem from "../PlayListItem/PlayListItem";
 import "./PlayList.scss";
+import playlists from "../../assets/Playlists.json";
 
 const PlayList = () => {
   return (
     <ul className="playlist">
-      <PlayListItem />
+      {playlists.playlists.items.map(({ external_urls, id, name }) => {
+        return <PlayListItem key={id} external_urls={external_urls} name={name} />;
+      })}
     </ul>
   );
 };
--- src/components/PlayListItem/PlayListItem.jsx
+++ src/components/PlayListItem/PlayListItem.jsx
@@ -1,17 +1,14 @@
 import React from "react";
-import playlists from "../../assets/Playlists.json";
 import "./PlayListItem.scss";
 
-const PlayListItem = () => {
-  return playlists.playlists.items.map(({ external_urls, id, name }) => {
-    return (
-      <li key={id}>
-        <a href={external_urls.spotify} className="playlist-item">
-          {name}
-        </a>
-      </li>
-    );
-  });
+const PlayListItem = ({ external_urls, name }) => {
+  return (
+    <li>
+      <a href={external_urls.spotify} className="playlist-item">
+        {name}
+      </a>
+    </li>
+  );
 };
 
 export default PlayListItem;
